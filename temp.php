c7153c841bfd255207df3150ec77acd02c8b8733
Leodiciommo <leozinho.ciommo@gmail.com>
2026-06-17 15:50:34 -0300

    arquivo temporário

--- temp.php
+++ temp.php
@@ -2,10 +2,18 @@
 
 require_once 'database.php';
 
+$db = new Database();
+$pdo = $db->getConnection();
+
 $stmt = $pdo->query("SHOW TABLES");
 
-echo "<h2>Tabelas:</h2>";
+echo "<pre>";
 
 while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-    echo $row[0] . "<br>";
+    echo $row[0] . "\n";
 }
+
+echo "</pre>";
+
+
+
